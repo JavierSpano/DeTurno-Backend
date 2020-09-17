1b7468b33ce96a7332ad2a6d96427337c62533b2
Emiliano <emiliano.arbos@istea.com.ar>
2020-09-16 22:41:11 -0300

    mapquestgeocodig y properties

--- src/main/java/com/javierfspano/deturno/services/MapquestGeoCodingServiceImpl.java
+++ src/main/java/com/javierfspano/deturno/services/MapquestGeoCodingServiceImpl.java
@@ -3,10 +3,18 @@ package com.javierfspano.deturno.services;
 import com.javierfspano.deturno.config.MapquestCredentials;
 import com.javierfspano.deturno.config.ProveedorDeCredenciales;
 import com.javierfspano.deturno.entities.respuestamapquest.Coordenadas;
+import com.javierfspano.deturno.entities.respuestamapquest.RespuestaMapquest;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class MapquestGeoCodingServiceImpl implements GeoCodingService {
+	@Value("${mapquestapi.key}")
+	private String KEY;
+	@Value("${mapquestapi.url}")
+	private String URL;
 
     private ProveedorDeCredenciales proveedorDeCredenciales;
 
@@ -18,6 +26,11 @@ public class MapquestGeoCodingServiceImpl implements GeoCodingService {
     public Coordenadas getCoordenadas(String direccion) {
         MapquestCredentials credentials = proveedorDeCredenciales.obtenerCredencialesDeMapquest();
 
-        return null;
+		RespuestaMapquest json = new RespuestaMapquest();
+		RestTemplate restTemplate = new RestTemplate();
+ 		json = restTemplate.getForObject(URL + KEY + "&location=" + direccion, RespuestaMapquest.class);
+
+		return json.getResults().get(0).getLocations().get(0).getLatLng();
+	
     }
 }
