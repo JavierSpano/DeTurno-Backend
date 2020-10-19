6a6240a00cf868527a1c80051f3aad46eda11420
emilianoarbos <37642344+emilianoarbos@users.noreply.github.com>
2020-10-18 23:11:50 -0300

    pruebas CI falla de build

--- src/main/java/com/javierfspano/deturno/DeTurnoApplication.java
+++ src/main/java/com/javierfspano/deturno/DeTurnoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeTurnoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeTurnoApplication.class, args);
+        SpringApplication.run(DeTurnoApplication.class, argssss);
     }
 
 }
