87f5f01170321d05716cba7e34ab569632b5cdb5
emilianoarbos <37642344+emilianoarbos@users.noreply.github.com>
2020-10-18 23:14:39 -0300

    se deja como estaba

--- src/main/java/com/javierfspano/deturno/DeTurnoApplication.java
+++ src/main/java/com/javierfspano/deturno/DeTurnoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeTurnoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeTurnoApplication.class, argssss);
+        SpringApplication.run(DeTurnoApplication.class, args);
     }
 
 }
